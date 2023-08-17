00cd13fc4a9f83cacaeb3cdab7808ae053eaa399
Alan Nato <alanwnato@gmail.com>
2023-08-17 18:58:35 +0000

    Let's do this for your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,49 +1,39 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""main app file for Flask instance in REST API
+"""
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
+import os
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
+
+
+def page_not_found(e):
+    """404 error json response"""
+    return jsonify({'error': "Not found"}), 404
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown_appcontext(exc=None):
+    """called on teardown of app contexts of flask
+    """
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
-
-
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    """run the app if the script is not being imported
+    """
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app.register_error_handler(404, page_not_found)
+    fetched_host = os.environ.get('HBNB_API_HOST')
+    fetched_port = os.environ.get('HBNB_API_PORT')
+    if fetched_host is None:
+        fetched_host = '0.0.0.0'
+    if fetched_port is None:
+        fetched_port = 5000
+    app.run(host=fetched_host, port=fetched_port, threaded=True)
